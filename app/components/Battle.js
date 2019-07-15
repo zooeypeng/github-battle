c75d77c90060703205a0a2a3741dd9ee18f1b445
zooeypeng <zooey.peng@gmail.com>
2019-07-15 13:42:00 +0800

    Results Skeleton

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { GiEgyptianWalk, GiBeamsAura, GiCrenelCrown, GiBroom } from 'react-icons/gi'
+import Results from './Results'
 
 function Instructions() {
   return (
@@ -127,7 +128,7 @@ function PlayerPreview({ username, onReset, label }) {
             className='btn-clear btn-reset'
             onClick={ onReset }>
             <GiBroom
-              color='darkgrey'
+              color='beige'
               size={ 20 }
             />
           </button>
@@ -149,7 +150,8 @@ export default class Battle extends React.Component {
 
     this.state = {
       playerOne: null,
-      playerTwo: null
+      playerTwo: null,
+      battle: false
     }
 
     this.handleSubmit = this.handleSubmit.bind(this)
@@ -169,7 +171,17 @@ export default class Battle extends React.Component {
   }
 
   render() {
-    const { playerOne, playerTwo } = this.state
+    const { playerOne, playerTwo, battle } = this.state
+
+    if (battle === true) {
+      return (
+        <Results
+          playerOne={ playerOne }
+          playerTwo={ playerTwo }
+        />
+      )
+    }
+
     return (
       <React.Fragment>
         <Instructions />
@@ -202,6 +214,13 @@ export default class Battle extends React.Component {
                 />
             }
           </div>
+          {playerOne && playerTwo && (
+            <button
+              className='btn btn-dark btn-space'
+              onClick={ () => { this.setState({ battle: true }) } }>
+              Battle
+            </button>
+          )}
         </div>
       </React.Fragment>
     )
---
+++ app/components/Results.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default class Results extends React.Component {
+  render() {
+    return(
+      <div>
+        <pre>{ JSON.stringify(this.props, null, 2) }</pre>
+      </div>
+    )
+  }
+}
