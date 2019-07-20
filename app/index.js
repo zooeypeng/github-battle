d3a6f772e5688c41289a66a7325998cde6d7754f
zooeypeng <zooey.peng@gmail.com>
2019-07-20 15:50:46 +0800

    use react context

--- app/index.js
+++ app/index.js
@@ -3,14 +3,30 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import Popular from './components/Popular'
 import Battle from './components/Battle'
+import { ThemeProvider } from './contexts/theme'
 
 class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      theme: 'light',
+      toggerTheme: () => {
+        this.setState(({ theme }) => ({
+          theme: theme === 'dark' ? 'light' : 'dark'
+        }))
+      }
+    }
+  }
+
   render() {
     return (
-      <div className="container">
-        <Battle />
-        {/* <Popular /> */}
-      </div>
+      <ThemeProvider value={ this.state }>
+        <div className="container">
+          <Battle />
+          {/* <Popular /> */}
+        </div>
+      </ThemeProvider>
     )
   }
 }
