5988c27165772c176e011f19f4c7997ecbb02f49
zooeypeng <zooey.peng@gmail.com>
2019-07-21 13:19:13 +0800

    Navbar: use NavLink

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -1,11 +1,35 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 import { ThemeConsumer } from '../contexts/theme'
+
+const activeStyle = {
+  color: 'goldenrod'
+}
 
 export default function Nav() {
   return (
     <ThemeConsumer>
       {({ theme, toggleTheme }) => (
         <nav className='row space-between'>
+          <ul className='row nav'>
+            <li>
+              <NavLink
+                exact
+                to='/'
+                activeStyle={ activeStyle }
+                className='nav-link'>
+                Popular
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to='/battle'
+                activeStyle={ activeStyle }
+                className='nav-link'>
+                Battle
+              </NavLink>
+            </li>
+          </ul>
           <button
             className='btn-clear'
             style={{ fontSize: 24 }}
