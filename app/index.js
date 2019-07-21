5a7a203b2d9a30c1303ce3bb3f0cddc8453fabef
zooeypeng <zooey.peng@gmail.com>
2019-07-21 17:09:09 +0800

    use router switch to build 404

--- app/index.js
+++ app/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { ThemeProvider } from './contexts/theme'
 import Nav from './components/Nav'
 import Popular from './components/Popular'
@@ -29,9 +29,12 @@ class App extends React.Component {
           <div className={ this.state.theme }>
             <div className="container">
               <Nav />
-              <Route exact path='/' component={ Popular } />
-              <Route exact path='/battle' component={ Battle } />
-              <Route path ='/battle/results' component={ Results } />
+              <Switch>
+                <Route exact path='/' component={ Popular } />
+                <Route exact path='/battle' component={ Battle } />
+                <Route path ='/battle/results' component={ Results } />
+                <Route render={ () => <h1>404</h1> } />
+              </Switch>
             </div>
           </div>
         </ThemeProvider>
